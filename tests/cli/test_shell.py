179464981920a683943919551ff4e2d87eeb1007
Weslley Morellato Bueno <wbueno@ebi.ac.uk>
2026-09-10 11:20:49 +0100

    Wire onedep-manager shell to launch OneDepTuiApp

--- tests/cli/test_shell.py
+++ tests/cli/test_shell.py
@@ -6,24 +6,24 @@ from onedep_manager.cli.common import CLIContext
 from onedep_manager.cli.shell import shell
 
 
-def test_shell_command_constructs_and_runs_cmdloop():
+def test_shell_command_constructs_and_runs():
     runner = CliRunner()
 
-    with mock.patch("onedep_manager.cli.shell.OneDepShell") as MockShell:
-        instance = MockShell.return_value
+    with mock.patch("onedep_manager.cli.shell.OneDepTuiApp") as MockApp:
+        instance = MockApp.return_value
         result = runner.invoke(shell, [], obj=CLIContext(config=mock.Mock()))
 
     assert result.exit_code == 0
-    instance.cmdloop.assert_called_once()
+    instance.run.assert_called_once()
 
 
 def test_shell_command_passes_site_option_through():
     runner = CliRunner()
 
-    with mock.patch("onedep_manager.cli.shell.OneDepShell") as MockShell:
+    with mock.patch("onedep_manager.cli.shell.OneDepTuiApp") as MockApp:
         runner.invoke(shell, ["--site", "WWPDB_DEPLOY_TEST_RU"], obj=CLIContext(config=mock.Mock()))
 
-    _, kwargs = MockShell.call_args
+    _, kwargs = MockApp.call_args
     assert kwargs["site"] == "WWPDB_DEPLOY_TEST_RU"
 
 
